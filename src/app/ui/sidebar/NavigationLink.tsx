28bd729df533af55be5b82fab899c6340ad5abe1
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-16 19:29:50 +0700

    product page

--- src/app/ui/sidebar/NavigationLink.tsx
+++ src/app/ui/sidebar/NavigationLink.tsx
@@ -2,41 +2,35 @@ import Link from 'next/link';
 
 export default function NavigationLink() {
   return (
-    <div className='flex flex-col '>
+    <div className='flex flex-col'>
       <Link
-        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-200/30 transition-all duration-300'
+        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-100/20 transition-all duration-300'
         href='/'
       >
-        home
+        Home
       </Link>
 
       <Link
-        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-200/30 transition-all duration-300'
+        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-100/20 transition-all duration-300'
         href='/store'
       >
         Books
       </Link>
       <Link
-        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-200/30 transition-all duration-300'
-        href='/track-order'
-      >
-        Track Order
-      </Link>
-      <Link
-        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-200/30 transition-all duration-300'
-        href='/store/communities'
-      >
-        Communities
-      </Link>
-      <Link
-        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-200/30 transition-all duration-300'
+        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-100/20 transition-all duration-300'
         href='/store/track-order'
       >
         Track Order
       </Link>
       <Link
-        className='px-4 text-md font-medium py-2 border-y-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-200/30 transition-all duration-300'
-        href='/store/about'
+        className='px-4 text-md font-medium py-2 border-t-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-100/20 transition-all duration-300'
+        href='/store/communities'
+      >
+        Communities
+      </Link>
+      <Link
+        className='px-4 text-md font-medium py-2 border-y-[1px] border-lime-200/80 w-full bg-transparent hover:bg-lime-100/20 transition-all duration-300'
+        href='/about'
       >
         About
       </Link>
