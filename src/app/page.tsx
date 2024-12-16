c65b47c7f909dab76fce31c541a53b2d259b929a
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-16 13:56:10 +0700

    pages done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+import bgImage from '../../public/home/bg-img.webp';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className='relative h-screen w-full flex flex-col justify-center items-center'>
+      <div className='w-10/12 md:w-7/12 mx-auto relative z-10 inset-0 bg-black/50  flex flex-col justify-center items-center p-10 rounded-lg bg-blur-xl space-y-4'>
+        <h1 className='text-white text-4xl font-bold text-center'>
+          Welcome to the Home Page
+        </h1>
+        <p className='text-white  text-center text-lg'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          quos.
+        </p>
+        <Link
+          href='/store'
+          className='bg-lime-500 text-black px-4 py-2 rounded-md hover:bg-lime-400 transition-all duration-300'
+        >
+          go to store
+        </Link>
+      </div>
+      <Image
+        className='absolute inset-0'
+        src={bgImage}
+        alt='bg-img'
+        fill
+        sizes='100vw'
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  );
 }
---
+++ src/app/store/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1>Store</h1>
+    </div>
+  );
+}
